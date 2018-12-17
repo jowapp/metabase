7f3a9f9e4b6bd65c722dade4babc863fdcec5c07
Antoine Maillard <antoine.maillard@gmail.com>
2018-12-17 16:32:34 +0100

    Weeks now start on Monday

--- src/metabase/query_processor/middleware/parameters/dates.clj
+++ src/metabase/query_processor/middleware/parameters/dates.clj
@@ -32,8 +32,9 @@
   [^DateTime start, ^DateTime end]
     ;; weeks always start on SUNDAY and end on SATURDAY
     ;; NOTE: in Joda the week starts on Monday and ends on Sunday, so to get the right Sunday we rollback 1 week
-   {:end   (.withDayOfWeek end DateTimeConstants/SATURDAY)
-    :start (.withDayOfWeek ^DateTime (t/minus start (t/weeks 1)) DateTimeConstants/SUNDAY)})
+    ;; EDIT PAR ANTOINE : support des semaines du lundi au dimanche
+   {:end   (.withDayOfWeek end DateTimeConstants/SUNDAY)
+    :start (.withDayOfWeek ^DateTime (t/minus start (t/weeks 1)) DateTimeConstants/MONDAY)})
 
 (defn- month-range
   [^DateTime start, ^DateTime end]
